1888a82ae9494c2fa6fc11b7f2ee2dbb1fd15268
Harry Crocksick <harrycrocksick1inamillion@gmail.com>
2023-09-19 14:02:05 +0630

    First release of Booking Site

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html'],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1200px',
+      '2xl': '1536px',
+    },
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      },
+      colors: {
+        'booking-red': '#d6293e',
+      },
+      keyframes: {
+        'fade-in-out': {
+          '0%, 100%': { opacity: '1' },
+          '50%': { opacity: '0' }
+        }
+      },
+      animation: {
+        'fade-in-out': 'fade-in-out 2500ms ease-in-out infinite'
+      }
+    },
+  },
+  plugins: [],
+}
