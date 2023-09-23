79dead8b113e75f692b543b9ac89be6ef7d943b3
Harry Crocksick <harrycrocksick1inamillion@gmail.com>
2023-09-24 00:28:10 +0630

    Added light/dark mode toggle switch

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import './style.css';
+
+const toggleBtn = document.querySelector('#toggle');
+toggleBtn.addEventListener('change', () => {
+  document.documentElement.classList.toggle('dark');
+});
