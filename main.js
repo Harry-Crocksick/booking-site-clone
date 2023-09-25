0523fd869b7c363b934679e6c2528e299eba7f6e
Harry Crocksick <harrycrocksick1inamillion@gmail.com>
2023-09-25 14:21:31 +0630

    Added dynamic date for copyright section

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import './style.css';
 
+const currentYear = document.querySelector('#year');
+const date = new Date();
+currentYear.innerHTML = date.getFullYear();
+
 const toggleBtn = document.querySelector('#toggle');
 toggleBtn.addEventListener('change', () => {
   document.documentElement.classList.toggle('dark');
